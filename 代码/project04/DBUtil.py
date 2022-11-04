0c80cc2d90fddc77cc1e91aa702f001617c0f355
chebnjsky <chebnjsky@skynj.com>
2022-11-04 16:54:24 +0800

    提交code

--- 代码/project04/DBUtil.py
+++ 代码/project04/DBUtil.py
@@ -1,11 +1,7 @@
 #!/usr/bin/env/python
 # coding=UTF-8
 '''
-@file          :   savemetricvalue.py
-@Author        :   程牧扬
-@Time          :   2022/11/4 14:38
-@Version       :   1.0
-@Describetion  :
+@Describetion  :数据公共模块
 '''
 import pymysql
 import prometheus_cib
@@ -38,31 +34,22 @@ class MYSQL():
 def prometheus():
     url = 'http://60.60.60.191:9090'
     expr = '{instance= "60.60.60.151:9101",job="151linux"} '
-    API = "/api/v1/query?query=" + expr
     result = prometheus_cib.queryAllPrometheusMetricValue(url, expr)
     return result
 
 
 if __name__ == '__main__':
-    sql = '''
-   drop table if exists prometheus;
-    create  table prometheus(
-    name varchar(10),
-    device varchar(10),
-    instance varchar(10),
-    job varchar(5),
-    value varchar(10)
-        )'''
+
     mysql = MYSQL(host=HOST, port=PORT, user=USER, password=PASSWORD, database=DATABASE)
-    # cursor.execute(sql)
     cursor = mysql.cursor
     for metric in prometheus():
         name = metric['metric']["__name__"]
         instance = metric['metric']["instance"]
         job = metric['metric']["job"]
-        value1 = (sum(float(value)) / len(metric['value']) for value in metric['value'])
-        value=str(metric['value'][1])
+        value1 = metric['value']
+        print(value1)
+        value = str(metric['value'][1])
         sql = f'insert into prometheus(name,instance,job,value)values("{name}","{instance}","{job}","{value}")'
         cursor.execute(sql)
         mysql.conn.commit()
-        print(name,instance,job,value)
+        # print(name,instance,job,value)
