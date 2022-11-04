bd977cea8dd84d6eb9fe3d2241386ef79ac3f39b
chebnjsky <chebnjsky@skynj.com>
2022-11-04 09:27:48 +0800

    delete CodeBase

--- 代码/project04/gateway.py
+++ 代码/project04/gateway.py
@@ -8,12 +8,26 @@
 @Describetion  :desc: WSGI服务器实现
 '''
 from wsgiref.simple_server import make_server
+from wsgiref.util import setup_testing_defaults
 from webservice import app
 
 
+def middleware():
+    pass
+
+
+def application(environ, start_response):
+    setup_testing_defaults(environ)
+    headers = [('Content-type', 'text/plain')]
+    status = '200 OK'.encode('utf-8')
+    start_response(status, headers)
+
+    return "HELLO,WORLD"
+
+
 def main():
-    sever = make_server('localhost', 8008, app)
-    sever.serve_forever()
+    httpd = make_server('localhost', 8008, app)
+    httpd.serve_forever()
 
 
 if __name__ == '__main__':
--- 代码/project04/prometheus_cib.py
+++ 代码/project04/prometheus_cib.py
@@ -10,25 +10,80 @@
 import requests
 
 URL = 'http://60.60.60.191:9090'
-response = requests.get(url=URL + '/api/v1/targets')
-try:
-    url = []
-    if response.status_code:
-        print(response.json()['data']['activeTargets'][1])
-        for discover in response.json()['data']['activeTargets']:
-            url.append(discover['scrapeUrl'])
-            text = response.text
-    print(url)
-    response1=requests.get(url=url[1])
-    textList=response1.text.split("\n")
-    print(len(textList))
-    for text in textList :
-        if text.startswith("#"):
-            textList.remove(text)
-    print(len(textList))
-    for text in textList:
-        print(text)
 
-except Exception as e:
-    print(123)
-    print(e)
+
+def prometheusQueryApi(url, API):
+    '''
+    :param url: 普罗米修斯地址
+    :param API: 访问的接口API
+    :return:
+    '''
+    return url + API
+
+
+def target_url():
+    response = requests.get(url=URL + '/api/v1/targets')
+    try:
+        url = []
+        if response.status_code:
+            print(response.json()['data']['activeTargets'][1])
+            for discover in response.json()['data']['activeTargets']:
+                url.append(discover['scrapeUrl'])
+                text = response.text
+    except Exception as e:
+        print(123)
+        print(e)
+
+
+def prometheus_metric(url):
+    API = "/metrics"
+    response = requests.get(url=url + API)
+    print(response.text)
+
+
+def prometheus_series(prometheus_rul, expr):
+    '''
+    通过标签匹配器查找系列
+    :return:
+    '''
+    API = '/api/v1/series?' + expr
+    response = requests.get(url=prometheus_rul + API)
+    print(response.text)
+
+
+def prometheus_query(prometheus_url, expr):
+    '''
+    :param prometheus_url: 普罗米修斯
+    :param expr:普罗米修斯表达式
+    :return:
+    '''
+    API = "/api/v1/query?query=" + expr
+    response = requests.get(url=prometheus_url + API)
+    result = response.text
+    print(result)
+
+
+# 查询指标元数据,
+def prometheus_query_metadata(prometheus_url, metric, limit=None):
+    '''
+    :param prometheus_url: 地址
+    :param metric: 查询指标
+    :param limit: 显示行数
+    :return:
+    '''
+    API = "/api/v1/metadata?limit=%s&metric=%s"(limit, metric)
+    response = requests.get(url=prometheus_url + API)
+    print(response.text)
+
+
+if __name__ == '__main__':
+    url = 'http://60.60.60.191:9090'
+    # prometheus_metric(url)
+    expr = "node_netstat_Tcp_RetransSegs{instance=“60.60.60.151:9101”，job=“151linux”}"
+    # prometheus_query(url, expr)
+    # 通过标签匹配器查找系列
+    # curl -g 'http://localhost:9090/api/v1/series?'
+    # --data-urlencode 'match[]=up'
+    # --data-urlencode 'match[]=process_start_time_seconds{job="prometheus"}'
+    expr2 = "--data-urlencode 'match[]=node_netstat_Tcp_RetransSegs' "
+    # prometheus_series(url, expr2)
