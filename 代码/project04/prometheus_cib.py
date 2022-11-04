f3965a39433a56ab2362db056dd0b52a7ed22599
chebnjsky <chebnjsky@skynj.com>
2022-11-04 10:56:40 +0800

    提交普罗米修斯采集代码

--- 代码/project04/prometheus_cib.py
+++ 代码/project04/prometheus_cib.py
@@ -41,26 +41,36 @@ def prometheus_metric(url):
     print(response.text)
 
 
+#    通过标签匹配器查找系列
 def prometheus_series(prometheus_rul, expr):
     '''
-    通过标签匹配器查找系列
     :return:
     '''
-    API = '/api/v1/series?' + expr
+    API = '/api/v1/series?' + " " + expr
+    queryAPI = prometheusQueryApi(prometheus_rul, API)
+    print(queryAPI)
     response = requests.get(url=prometheus_rul + API)
     print(response.text)
 
 
+# 普罗米修斯 查询
 def prometheus_query(prometheus_url, expr):
     '''
     :param prometheus_url: 普罗米修斯
     :param expr:普罗米修斯表达式
     :return:
     '''
+    metric = 'node_memory_MemTotal_bytes'
     API = "/api/v1/query?query=" + expr
     response = requests.get(url=prometheus_url + API)
-    result = response.text
+    result = response.json()
     print(result)
+    if result['status'] == 'success':
+        dataset = result['data']['result']
+        metric_value = dataset[0]['value']
+        total_value = sum(float(value) for value in metric_value)
+        #格式转化 将byte转接为MB
+        print(total_value / len(metric_value) / 1024 / 1024 )
 
 
 # 查询指标元数据,
@@ -71,7 +81,7 @@ def prometheus_query_metadata(prometheus_url, metric, limit=None):
     :param limit: 显示行数
     :return:
     '''
-    API = "/api/v1/metadata?limit=%s&metric=%s"(limit, metric)
+    API = "/api/v1/metadata?limit=%s&metric=%s" % (limit, metric)
     response = requests.get(url=prometheus_url + API)
     print(response.text)
 
@@ -79,11 +89,14 @@ def prometheus_query_metadata(prometheus_url, metric, limit=None):
 if __name__ == '__main__':
     url = 'http://60.60.60.191:9090'
     # prometheus_metric(url)
-    expr = "node_netstat_Tcp_RetransSegs{instance=“60.60.60.151:9101”，job=“151linux”}"
-    # prometheus_query(url, expr)
+    expr = 'node_memory_MemTotal_bytes{instance="60.60.60.151:9101",job="151linux"}'
+    prometheus_query(url, expr)
     # 通过标签匹配器查找系列
     # curl -g 'http://localhost:9090/api/v1/series?'
     # --data-urlencode 'match[]=up'
     # --data-urlencode 'match[]=process_start_time_seconds{job="prometheus"}'
-    expr2 = "--data-urlencode 'match[]=node_netstat_Tcp_RetransSegs' "
+    # expr2 = " --data-urlencode 'match[]=node_netstat_Tcp_RetransSegs' "
+    # expr2 = '''--data-urlencode 'match[]=up' --data-urlencode 'match[]=process_start_time_seconds{job="
+    # prometheus
+    # "}'''
     # prometheus_series(url, expr2)
